2990a585b7571978d397276f2d2e11a9bb3288fb
Paul Fischer <paulfischer@Pauls-Air.fios-router.home>
2017-11-28 21:14:46 -0500

    scaffolds feature modules

---
+++ src/app/weather/weather.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WeatherRoutingModule } from './weather-routing.module';
+import { WeatherComponent } from './weather.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    WeatherRoutingModule
+  ],
+  declarations: [WeatherComponent]
+})
+export class WeatherModule { }
